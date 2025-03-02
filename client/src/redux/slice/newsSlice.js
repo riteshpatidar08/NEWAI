5c54710bad02aa0bb0270f075a22dada1a6495bb
Ritesh <itsmeriteshpatidar@gmail.com>
2025-03-02 10:11:29 +0530

    updated till the gauth

--- client/src/redux/slice/newsSlice.js
+++ client/src/redux/slice/newsSlice.js
@@ -1,34 +1,49 @@
-import { createSlice ,createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { getCookie } from "../../utils/utils";
 import axios from "axios";
 
 const initialState = {
-loading : false
-}
+  loading: false,
+  data: null,
+  error: null,
+};
 
-const id = getCookie('id') ;
-console.log(id)
-
-export const setPreferences = createAsyncThunk('/preferences', async(data, {rejectWithValue})=>{
-try {
-    const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/preferences/${id}`,data)
-    return res.data
-} catch (error) {
-    return rejectWithValue(error)
-}
-})
+export const setPreferences = createAsyncThunk(
+  "preferences/setPreferences",
+  async (data , { rejectWithValue }) => {
+  
+    const id = getCookie('id')
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/preferences/${id}`,
+        data
+      );
+      return res.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "Something went wrong");
+    }
+  }
+);
 
 const newsSlice = createSlice({
-name : "news",
-initialState,
-extraReducers : (builder) => {
-builder.addCase(setPreferences.pending, (state)=>{
-    state.loading = true
-}).addCase(setPreferences.fulfilled,(state)=>{
-    state.loading = false
-})
-}
-})
+  name: "news",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(setPreferences.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(setPreferences.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(setPreferences.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
 
-
-export default newsSlice.reducer
+export const { resetState } = newsSlice.actions;
+export default newsSlice.reducer;
