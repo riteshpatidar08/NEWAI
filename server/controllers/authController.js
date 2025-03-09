37e8ae95ea80eb416d2f3ab166d8eb365f808414
Ritesh <itsmeriteshpatidar@gmail.com>
2025-03-09 23:23:47 +0530

    fd

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,11 +27,12 @@ export const login = async (req, res) => {
       { expiresIn: '1d' }
     );
 
-    res.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 15 * 24 * 60 * 60 * 1000,
-      sameSite : 'strict'
-    });
+ res.cookie('token', token, {
+  httpOnly: true,
+  maxAge: 15 * 24 * 60 * 60 * 1000,
+  sameSite: 'none',
+  secure: true
+});
 
     res.status(200).json({
       preferences: user.preferences,
